6aa06eaf54018a698a1349dd9ee1ac88dac1298c
Regina Garcia <regina7361@gmail.com>
2019-02-28 21:05:12 -0800

    testing

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+//array of objects - questions and answers
+var triviaQuestions = [
+    {
+        question: "Who sang - Sure Be Cool If You Did",
+        rightAnswer: "Blake Shelton",
+        wrongAnswer: "Brad Paisley", "Reba MacIntyre", "Luke Bryan",
+    },
+    {
+        question: "Who sang - Belongs to You",
+        rightAnswer: "Emerson Drive",
+        wrongAnswer: "Brad Paisley", "Reba MacIntyre", "Luke Bryan",
+    },
+    {
+        question: "Who sang - Two Black Cadillacs",
+        rightAnswer: "Carrie Underwood",
+        wrongAnswer: "Brad Paisley", "Reba MacIntyre", "Luke Bryan",
+    },
+    {
+        question: "Who sang - Lost in This Moment",
+        rightAnswer: "Big & Rich",
+        wrongAnswer: "Brad Paisley", "Reba MacIntyre", "Luke Bryan",
+    },
+    {
+        question: "Who sang - Best Days of Your Life",
+        rightAnswer: "Kelly Pickler",
+        wrongAnswer: "Brad Paisley", "Reba MacIntyre", "Luke Bryan",
+    },
+ ]
+//for loop to call the array
+//start timer function
+//radio buttons
+//submit button
